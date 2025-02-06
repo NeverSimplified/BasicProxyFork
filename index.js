2ed04edd5d2a23fe7a7d56252da3252fb0754333
NeverSimplified <93670398+NeverSimplified@users.noreply.github.com>
2025-02-06 20:48:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -34,20 +34,22 @@ const domains = [
     "users"
 ]
 
+addEventListener("fetch", event => {
+    event.respondWith(fetch(event.request))
+})
+
 // Export our request handler
-export default {
-    async fetch(request) {
-        const url = new URL(request.url);
-        const path = url.pathname.split(/\//);
+async function fetch(request) {
+    const url = new URL(request.url);
+    const path = url.pathname.split(/\//);
+
+    if (!path[1].trim()) return new Response(JSON.stringify({ message: "Missing ROBLOX subdomain." }), { status: 400 });
+
+    if (!domains.includes(path[1])) return new Response(JSON.stringify({ message: "Specified subdomain is not allowed." }), { status: 401 });
     
-        if (!path[1].trim()) return new Response(JSON.stringify({ message: "Missing ROBLOX subdomain." }), { status: 400 });
-    
-        if (!domains.includes(path[1])) return new Response(JSON.stringify({ message: "Specified subdomain is not allowed." }), { status: 401 });
-        
-        return fetch(`https://${path[1]}.roblox.com/${path.slice(2).join("/")}${url.search}`, {
-            method: request.method,
-            headers: request.headers["content-type"] ? { "Content-Type": request.headers["content-type"] } : {},
-            body: request.body
-        });
-    }
+    return fetch(`https://${path[1]}.roblox.com/${path.slice(2).join("/")}${url.search}`, {
+        method: request.method,
+        headers: request.headers["content-type"] ? { "Content-Type": request.headers["content-type"] } : {},
+        body: request.body
+    });
 }
